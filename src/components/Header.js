e3f5e28a63715e8e0ab74fb7c64adbd233137939
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-03-14 18:33:07 +0100

    header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../images/logo.svg'
 
 function Header() {
   return (
-    <header className='flex justify-center'>
+    <header className='flex justify-center pt-20 pb-10'>
         <img src={logo} alt="" />
     </header>
   )
