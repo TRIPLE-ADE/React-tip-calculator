57ec2a7018090af3d621fef9f2714b0c99a7698c
Jay-Fred31 <70653973+Jay-Fred31@users.noreply.github.com>
2023-03-15 15:36:45 +0100

    added Button component and edited form component to include the buttons

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function TipButton({percentValue}) {
+  <button
+    type="button"
+    className="px-3 py-2 text-white bg-Very-dark-cyan hover:bg-Strong-cyan"
+  >
+    {percentValue}
+  </button>;
+}
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Input from './Input'
+import { TipButton } from './Button'
 
 function Form() {
   return (
     <form action="">
-        <Input name="Bill"/>
-        <Input name="Number of People"/>
+      <Input name="Bill" />
+      <div className="grid-col-3 gap-x-2">
+        <TipButton percentValue={5} />
+        <TipButton percentValue={10} />
+        <TipButton percentValue={15} />
+        <TipButton percentValue={25} />
+        <TipButton percentValue={50} />
+        <Input name='custom'/>
+      </div>
+      <Input name="Number of People" />
     </form>
-  )
+  );
 }
 
 export default Form
