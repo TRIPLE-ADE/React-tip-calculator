e8411ad8ccd545b9a8d772f5ade6c64818d06a2f
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-03-16 05:54:31 +0100

    input styling

--- src/components/Output.js
+++ src/components/Output.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function Output({label, amount}) {
+function Output({ label, amount }) {
   return (
     <div>
-        <div>
-            <p>{label}</p>
-            <p>/Person</p>
-        </div>
-        <p>{amount}</p>
+      <div>
+        <p>{label}</p>
+        <p>/Person</p>
+      </div>
+      <p>{amount}</p>
     </div>
-  )
+  );
 }
 
-export default Output
+export default Output;
