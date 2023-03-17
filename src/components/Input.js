1958e26ed65d79302150aafa45038a6e4d411455
Jay-Fred31 <70653973+Jay-Fred31@users.noreply.github.com>
2023-03-17 15:49:50 +0100

    edited the tip button  and added backgroung to the icons

--- src/components/Input.js
+++ src/components/Input.js
@@ -6,6 +6,7 @@ export function CustomInput() {
       type="number"
       name="custom"
       min="0"
+      max="50"
       placeholder="Custom"
       className="block text-center font-bold placeholder:text-primary-Dark-grayish-cyan bg-primary-Very-light-grayish-cyan focus:outline-none"
     />
@@ -15,10 +16,16 @@ export function CustomInput() {
 
 function Input({name, icon}) {
   return (
-    <div className='grid gap-1'>
-      <label className='text-primary-Dark-grayish-cyan text-sm font-bold'>{name}</label>
-      <div className='relative'>
-        <img src={icon} alt="" className="absolute top-3 left-3" />
+    <div className="grid gap-1">
+      <label className="text-primary-Dark-grayish-cyan text-sm font-bold">
+        {name}
+      </label>
+      <div className="relative">
+        <img
+          src={icon}
+          alt=""
+          className="absolute top-3 px-2 left-3 bg-primary-Very-light-grayish-cyan"
+        />
         <input
           type="number"
           name={name}
